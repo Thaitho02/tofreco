44a34cc1ca56502d19859a5a9dbc65e2b0c4befe
quypn <phamngocquy97@gmail.com>
2024-05-15 22:16:28 +0700

    fix sytax

--- fixfile.C
+++ fixfile.C
@@ -1,52 +1,51 @@
-void c()
-{
-    TChain *tIn = new TChain("TofHitsTree");
-    tIn->Add("/home/thaitho/tof/output/run16573_outputTree.root");
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TH2F.h"
 
-    std::vector<Double_t> *SignalTime = nullptr;
-    std::vector<Double_t> *SignalPlane = nullptr;
+void fixfile() {
+  TChain *tIn = new TChain("TofHitsTree");
+  tIn->Add("/home/qpham/IProject/tofreco/run16573_outputTree.root");
 
-    tIn->SetBranchAddress("SignalPosition", &SignalPosition);
-    tIn->SetBranchAddress("SignalPlane", &SignalPlane);
+  std::vector<Double_t> *SignalTime = nullptr;
+  std::vector<Double_t> *SignalPlane = nullptr;
+  std::vector<Double_t> *SignalPosition = nullptr;
 
-    // Khởi tạo histogram
-    TH2F *h = new TH2F("h", "Signal Position Graph", 100, 0, 10000000000, 100, 0, 10000000000);
+  tIn->SetBranchAddress("TofHitsTree.SignalPosition", &SignalPosition);
+  tIn->SetBranchAddress("TofHitsTree.SignalPlane", &SignalPlane);
 
-    Long64_t nEntries = tIn->GetEntries();
+  // Khởi tạo histogram
+  TH2F *h = new TH2F("h", "Signal Position Graph", 100, 0, 10000000000, 100, 0,
+                     10000000000);
 
-    for (Long64_t i = 0; i < nEntries; i++)
-    {
-        tIn->GetEntry(i);
+  Long64_t nEntries = tIn->GetEntries();
 
-        // Lưu trữ tọa độ x và y
-        Double_t xPos = 0.0;
-        Double_t yPos = 0.0;
-        
-        // Tìm tọa độ x của SignalPlane 3 và tọa độ y của SignalPlane 4
-        for (size_t j = 0; j < SignalTime->size(); j++)
-        {
-            if ((*SignalPlane)[j] == 2)
-            {
-                xPos = (*SignalTime)[j];
-            }
-            else if ((*SignalPlane)[j] == 3)
-            {
-                yPos = (*SignalTime)[j];
-            }
+  for (Long64_t i = 0; i < nEntries; i++) {
+    tIn->GetEntry(i);
 
-            h->Fill(xPos, yPos);
-        }
+    // Lưu trữ tọa độ x và y
+    Double_t xPos = 0.0;
+    Double_t yPos = 0.0;
 
-        // Fill histogram với tọa độ x và y
-     
+    // Tìm tọa độ x của SignalPlane 3 và tọa độ y của SignalPlane 4
+    for (size_t j = 0; j < SignalTime->size(); j++) {
+      if ((*SignalPlane)[j] == 2) {
+        xPos = (*SignalTime)[j];
+      } else if ((*SignalPlane)[j] == 3) {
+        yPos = (*SignalTime)[j];
+      }
+
+      h->Fill(xPos, yPos);
     }
 
-    // Tạo một canvas và vẽ histogram
-    TCanvas *c = new TCanvas("c", "Canvas", 800, 600);
-    h->Draw("COLZ"); // Sử dụng "COLZ" để vẽ histogram 2D
+    // Fill histogram với tọa độ x và y
+  }
 
-    c->SaveAs("SignalTimeGraph.png");
+  // Tạo một canvas và vẽ histogram
+  TCanvas *c = new TCanvas("c", "Canvas", 800, 600);
+  h->Draw("COLZ"); // Sử dụng "COLZ" để vẽ histogram 2D
 
-    delete tIn; // Giải phóng bộ nhớ
-    delete h;
+  c->SaveAs("SignalTimeGraph.png");
+
+  delete tIn; // Giải phóng bộ nhớ
+  delete h;
 }
